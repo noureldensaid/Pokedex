c730535a6649d4254205fa662b42849bf0a765b7
nourr <nourmorgan19@gmail.com>
2023-04-28 08:00:58 +0200

    setup retrofit

--- app/src/main/java/com/fyp/pokedex/data/repositorey/Repository.kt
+++ app/src/main/java/com/fyp/pokedex/data/repositorey/Repository.kt
@@ -1,23 +1,38 @@
 package com.fyp.pokedex.data.repositorey
 
+import android.util.Log
 import com.fyp.pokedex.data.remote.PokeApi
+import com.fyp.pokedex.models.pokemon.Pokemon
+import com.fyp.pokedex.models.pokemonList.PokemonList
+import com.fyp.pokedex.util.Resource
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class Repository @Inject constructor(
-    val pokeApi: PokeApi
+    private val api: PokeApi
 ) {
 
+    suspend fun getPokemonList(limit: Int, offset: Int): Resource<PokemonList> {
+        val response = try {
+            api.getPokemonList(limit, offset)
+        } catch (e: Exception) {
+            Log.e("Error", "getPokemonList: $e")
+            return Resource.Error("Can't fetch pokemon list" + e.message)
+        }
+        return Resource.Success(response)
+    }
 
 
-
-
-
-
-
-
-
+    suspend fun getPokemonInfo(name: String): Resource<Pokemon> {
+        val response = try {
+            api.getPokemonInfo(name)
+        } catch (e: Exception) {
+            Log.e("Error", "getPokemonInfo: $e")
+            return Resource.Error("Can't fetch pokemon list")
+        }
+        return Resource.Success(response)
+    }
 
 
 }
